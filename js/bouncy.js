9af5397fad0eb67f5a0af9ef1591c5e13f63b3a9
Marc Fawzi <marc.fawzi@gmail.com>
2013-03-16 18:41:40 -0700

    First pages commit

---
+++ js/bouncy.js
@@ -0,0 +1,235 @@
+/*
+
+  SVG Bouncing Ball using OrganicJS
+
+  Written as a coding exercise with robust design for more complex scenario
+
+  @MarcFawzi 2013
+
+ */
+
+(function() {
+
+    app.board = function Board(cache, model) {
+
+        var obj = {};
+
+        obj.props = {};
+
+        // explicitly set to undefined so they may be found by the build function
+        obj.props.top = undefined;
+        obj.props.left = undefined;
+        obj.props.width = undefined;
+        obj.props.height = undefined;
+        obj.props.fill = undefined;
+        obj.props.elem = undefined;
+
+        app.build(obj, cache, model);
+
+        obj.render = function () {
+
+            var elem = obj.elem()[0] || obj.elem()
+
+            elem.setAttributeNS(null, "x", obj.left());
+
+            elem.setAttributeNS(null, "y", obj.top());
+            elem.setAttributeNS(null, "width", obj.width());
+            elem.setAttributeNS(null, "height", obj.height());
+            elem.setAttributeNS(null, "fill", obj.fill());
+
+            return obj;
+        }
+
+        obj.onClick = function(func) {
+
+            $(obj.elem()).bind("click", func)
+
+            return obj;
+        }
+
+        return obj;
+    }
+
+
+    app.slider = function Slider(cache, model) {
+
+        var obj = {};
+
+        obj.props = {};
+
+        // explicitly set to undefined so they may be found by the build function
+        obj.props.elem = undefined
+        obj.props.left = undefined
+        obj.props.top = undefined
+        obj.props.display = undefined
+        obj.props.min = undefined
+        obj.props.max = undefined
+        obj.props.value = undefined
+
+        app.build(obj, cache, model);
+
+        obj.onChange = function(func) {
+
+            $(obj.elem()).bind("change", func)
+
+            return obj;
+        }
+
+        obj.render = function() {
+
+            var elem = obj.elem()[0] || obj.elem()
+
+            elem.setAttributeNS(null, "min", obj.min())
+            elem.setAttributeNS(null, "max", obj.max())
+            elem.setAttributeNS(null, "value", obj.value())
+
+            $(elem).css({"margin-left": obj.left(), "margin-top": obj.top(), display: obj.display()})
+
+            return obj;
+        }
+
+        return obj;
+    }
+
+    app.ball = function Ball(cache, model) {
+
+       var rChange = 0;
+
+       var obj = {};
+
+       obj.props = {};
+
+       // explicitly set to undefined so they may be found by the build function
+       obj.props.radius = undefined;
+       obj.props.dx = undefined;
+       obj.props.dy = undefined;
+       obj.props.speed = undefined;
+       obj.props.x = undefined;
+       obj.props.y = undefined;
+       obj.props.fill = undefined;
+       obj.props.animating = undefined;
+       obj.props.elem = undefined;
+
+       app.build(obj, cache, model);
+
+       obj.render = function() {
+
+           var elem = obj.elem()[0] || obj.elem()
+
+           elem.setAttributeNS(null, "r", obj.radius());
+           elem.setAttributeNS(null, "cx", obj.x());
+           elem.setAttributeNS(null, "cy", obj.y());
+           elem.setAttributeNS(null, "fill", obj.fill());
+
+           return obj;
+       }
+
+       obj.animate = function() {
+
+           animID = window.requestAnimationFrame(obj.animate)
+
+           var elem = obj.elem()[0] || obj.elem()
+
+           obj.x((obj.x() + obj.dx() * obj.speed()));
+           obj.y((obj.y() + obj.dy() * obj.speed()));
+
+           elem.setAttributeNS(null, "cx", obj.x())
+           elem.setAttributeNS(null, "cy", obj.y())
+
+           // In OrganicJS, components do not depend on other components directly but on anonymous data and behavior
+           // from other components that are cached in an isolated context.
+           //
+           // Contextual Caching provides a soft, decoupled kind of dependency, allowing any component to serve as the
+           // dependency as long as it offers the expected data and/or behavior. Component caching isolates the 
+           // dependency from the actual component in both time and space, using 'widget context/component name' as 
+           // the path, where context is the cached component's .widget() property defined by the user (for
+           // a set of collaborating components within a widget) and name is the cached component's .provide() which 
+           // is available to the consuming component via that component's .consume() map.
+           //
+           // Cached components behave exactly the same as regular components, except they're cloned from the
+           // component at the time of caching so they have different state and a different JS native-object
+           // context (not to be confused with the component's user-specified context)
+           //
+           // In case no consumable components are found (in this case the 'board' in the component's widget context 
+           // was consumed in 'animate' method) then replace with the expected data/behaviors
+
+           var consumed;
+
+           // find out what name was imported ('animate' being this method's name)
+           for (var n = 0; n < obj.consume().length; n++) {
+
+               if (obj.consume()[n].into = "animate") {
+                   consumed = obj.consume()[n].name;
+                   break;
+               }
+           }
+
+           // try to find a component in cache that has that name
+           var cachedObject = cache.get(obj.cache() + "/" + consumed) ?
+                        cache.get(obj.cache() + "/" + consumed) :
+           // else replace with the expected data/behaviors
+                        {
+                            left: function() {return 0},
+                            top: function () {return 0},
+                            width: function() {return 400},
+                            height: function() {return 400}
+                        }
+
+           // to bounce it, mirror the ball's path after it runs into an edge
+
+           // left or right
+           if (obj.x() + obj.dx() > cachedObject.width() - obj.radius() - rChange + cachedObject.left() ||
+
+               obj.x() + obj.dx() < obj.radius() + rChange + cachedObject.left())
+
+                    obj.dx(-obj.dx());
+
+           // top or bottom
+           if (obj.y() + obj.dy() < obj.radius() + rChange + cachedObject.top() ||
+
+               obj.y() + obj.dy() > cachedObject.height() - obj.radius() - rChange + cachedObject.top())
+
+           {
+
+               obj.dy(-obj.dy());
+           }
+
+           obj.animating(true);
+
+           return obj;
+
+       }
+
+       return obj;
+    }
+
+    // Polyfill source: Opera developer's blog
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    var animID;
+
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+            window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+
+
+}())
+
+
---
+++ js/organic.js
@@ -0,0 +1,263 @@
+/*
+
+ OrganicJS is a reusable component pattern and feather-weight micro-framework
+
+ OrganicJS supports reusable components with chain*able properties and public methods, dynamic setters/getters,
+ reusable widget markup, using nestable HTML/SVG fragments with in-place cloning and rendering, and
+ sharing of data/behavior among components in a soft, decoupled manner, using contextual component caching.
+
+ @MarcFawzi
+
+ Last modified: MARCH 16, 2013
+
+ v0.2
+
+ */
+
+(function(){
+
+    var app = app || {};
+
+    // Getters/Setters
+    // this creates getter/setter on the object for each of its public properties
+    // so that public properties may be accessed as componentInstance.property(value) (when setting)
+    // and componentInstance.property() (when getting)
+    app.build = app.build || function Build(obj, cache, model)  {
+
+        if (obj.props === undefined)
+            throw new Error("no public properties defined").stack;
+
+        //explicitly set to undefined so it can be found in props
+        obj.props.cache = undefined;
+        obj.props.model = undefined;
+        obj.props.consume = undefined;
+        obj.props.provide = undefined;
+
+        for (var o in obj.props) {
+            obj[o] =  new Function("value", "if (!arguments.length) return typeof this.props['" + o +
+                "'] == 'function' ? this.props['" + o + "'].call(this) : this.props['" + o + "'];" +
+                "this.props['" + o + "'] = (typeof value == 'function' ? value.call(this) : value); return this")
+        }
+
+        // special setter/getter
+        obj.provide = function(name) {
+
+            if (name !== undefined) {
+                obj.props.provide = name
+
+                // Contextual Component Cache
+                // store the component using "cash context/component name" as the path (key)
+                cache.set(obj.cache() + "/" + name, obj);
+
+                return obj;
+            }
+            else {
+                return obj.props.provide;
+            }
+        }
+    }
+
+    // Contextual Component Cache
+    //
+    // For caching and sharing component's data and behavior with components that have the same user-defined context
+    //
+    // Contextual Component Cache clones the component and saves the clone (with its data and behaviors operating in
+    // the cloned context) under the 'cash context/component name' path where context is the component .cache()
+    // property while name is specified in the .provide() property of the component being provided, and available 
+    // to the consuming component via that component's .consume() property.
+    //
+    // use .provide(name) to cache component
+    // use .consume([{name: 'component name', into: 'method or property'}, { ... }, etc]) to consume cached component
+    // use console.log(cache.contexts()) to view all cached objects and their properties/methods, in all cash contexts
+    //
+    //
+    app.cache = app.cache || function Cache() {
+
+        var obj = {}
+
+        obj.contexts = function() { return obj}
+
+        // cache the component in the given 'cash context/ component name' path
+        obj.set = function(context$name, comp) {
+
+            obj[context$name] = {};
+
+            for (key in comp) {
+                obj[context$name][key] = comp[key]
+            }
+        }
+
+        // return component if it exists in cache in the given context/name
+        obj.get = function(context$name) {
+            return obj[context$name];
+        }
+
+        return obj;
+    }
+
+    // Scoped Data Model
+    //
+    // For sharing independent data structures between widgets that have the same scope
+    //
+    // Scoped Model offers the ability to create, access and update independent data structures within a given
+    // scope that may transcend the cash context.
+    //
+    // use model("scope").create({key1: value, key2: value, etc}) to create new properties
+    // use model("scope").keyName() to get the value for a key in the given scope
+    // use model("scope").keyName(value) to set the value for a key in the given scope
+    // use console.log(model.scopes()) to view all scopes and keys within them
+    //
+    // setters are chain-able
+    //
+    // the scope specified and returned by the component's .model()
+
+    app.model = app.model || function Model(scope) {
+
+        var obj = {}
+
+        obj.scopes = function() { return obj}
+
+        obj.create = function(json) {
+
+            if (typeof obj[scope] == 'undefined')
+                throw new Error("no scope defined").stack
+
+            obj[scope] = obj[scope] || {}
+
+            obj[scope].props = obj[scope].props || {}
+
+            for (var key in json) {
+                obj[scope].props[key] = json[key]
+            }
+
+            for (var o in obj[scope].props) {
+                if (obj[scope].props.hasOwnProperty(o)) {
+                    obj[o] =  new Function("scope, value", "if (!scope) return; if (!value) " +
+                        "return this[scope].props['" + o + "'];" +
+                        "this[scope].props['" + o +
+                        "'] = value; return this")
+                }
+            }
+            return obj;
+        }
+        return obj;
+    }
+
+    // in-place cloning of HTML/SVG fragments
+    app.new = app.new || function Frig(frag) {
+
+        // jQuery to native
+        var el = frag[0] || frag;
+
+        // make sure the node exists
+        if (!el)
+            throw new Error("Can't find node").stack
+
+        // make sure it's an element
+        if (el.nodeType != 1)
+            throw new Error("Can't use this node type").stack
+
+        var node = el.cloneNode(true)
+
+        if (typeof node.getAttributeNS(null, "id") != 'undefined')
+            node.removeAttributeNS(null, "id")
+
+        if (typeof node.getAttributeNS(null, "frag") != 'undefined')
+            node.removeAttributeNS(null, "frag")
+
+        var uuid = app.uuid();
+
+        node.setAttributeNS(null, "uuid", uuid)
+
+        // insert new instance in place and remove original fragment
+
+        el.parentNode.appendChild(node)
+
+        // return element's uuid
+        return uuid;
+    }
+
+
+    // save HTML/SVG of fragment prior to cloning
+    // only needs to be called once per top-most fragment
+    app.save = app.save || function Save(frag) {
+
+        // jQuery to native
+        var el = frag[0] || frag;
+
+        el._clone = el._clone || el.cloneNode(true);
+
+    }
+
+    // restore HTML/SVG of previously saved fragment
+    app.restore = app.restore || function Restore(frag) {
+
+        // jQuery to native
+        var el = frag[0] || frag;
+
+        var parentNode = el.parentNode;
+
+        var clone = el._clone.cloneNode(true)
+
+        clone._clone = clone.cloneNode(true)
+
+        parentNode.appendChild(clone)
+
+        parentNode.removeChild(el)
+
+    }
+
+    app.getElementByUUID = app.getElementByUUID || function GetElementByUUID(uuid) {
+
+        return document.querySelector('[uuid="' + uuid + '"]')
+    }
+
+    app.uuid = app.uuid ||
+
+        (typeof(window.crypto) != 'undefined' &&
+            typeof(window.crypto.getRandomValues) != 'undefined') ?
+
+        function UUID() {
+            // If we have a cryptographically strong PRNG
+            // http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
+            var buf = new Uint16Array(8);
+            window.crypto.getRandomValues(buf);
+            var S4 = function(num) {
+                var ret = num.toString(16);
+                while(ret.length < 4){
+                    ret = "0"+ret;
+                }
+                return ret;
+            };
+
+            return (S4(buf[0])+S4(buf[1])+"-"+S4(buf[2])+"-"+S4(buf[3])+"-"+S4(buf[4])+"-"+
+                    S4(buf[5])+S4(buf[6])+S4(buf[7]));
+        }
+
+        :
+
+        // Else limit chance of collisions to scenarios where calls to app.uuid() happen within the same millisecond
+        // and use the same Math.random seed, which is only possible when different users using the same browser or
+        // same Math.random implementation) load the app at the same exact millisecond in UTC time (the seed to
+        // Math.random), which is of no concern for OrganicJS element UUID because the context of the PRNG collisions
+        // is limited in the local window.document object.
+        //
+        // For other uses, e.g. generating any IDs above user/session scope to save on the server, you should avoid
+        // using app.uuid() if the target browser does not support window.crypto
+
+        function UUID() {
+            var now = new Date();
+            var ms = now.getTime() + (now.getTimezoneOffset() * 60000)
+
+            var uuid = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) {
+                var r = (ms + Math.random()*16)%16 | 0;
+                ms = Math.floor(ms/16);
+                return (r).toString(16);
+            });
+
+            return uuid;
+        };
+
+    window.app = app;
+
+} ())
